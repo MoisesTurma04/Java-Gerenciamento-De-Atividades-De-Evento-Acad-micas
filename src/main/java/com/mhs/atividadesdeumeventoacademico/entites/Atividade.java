66c8b3ad432bd3f04e99046836efcd8b3576f391
Moises Hastenraiter Da Silva <moiseshasten@gmail.com>
2024-04-14 21:49:43 -0300

    Adicionadas implementações das entidades Atividade, Bloco, Categoria e Participante. Relacionamentos entre as entidades definidos e mapeados corretamente. Atualizações feitas no arquivo de configuração application.properties.

--- src/main/java/com/mhs/atividadesdeumeventoacademico/entites/Atividade.java
+++ src/main/java/com/mhs/atividadesdeumeventoacademico/entites/Atividade.java
@@ -1,9 +1,17 @@
 package com.mhs.atividadesdeumeventoacademico.entites;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,4 +24,74 @@ public class Atividade {
 	private String nome;
 	private String descricao;
 	private Double preco;
+
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+
+	@ManyToOne
+	@JoinColumn(name = "atividade_id")
+	private Atividade atividade;
+
+	@ManyToMany(mappedBy = "atividades")
+	private Set<Participante> participantes;
+
+	@OneToMany(mappedBy = "atividade")
+	private List<Bloco> blocos = new ArrayList<>();
+
+	public Atividade() {
+	}
+
+	public Atividade(Integer id, String nome, String descricao, Double preco, Atividade atividade) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.atividade = atividade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Atividade getAtividade() {
+		return atividade;
+	}
+
+	public void setAtividade(Atividade atividade) {
+		this.atividade = atividade;
+	}
+
+	public List<Bloco> getBlocos() {
+		return blocos;
+	}
+
 }
